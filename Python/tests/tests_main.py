b9c5f56f30b2f5924f338c7d19a132f3207aa2e3
JesperMjornman <jesper.mjornman@gmail.com>
2020-10-24 14:25:00 +0200

    fix test

--- Python/tests/tests_main.py
+++ Python/tests/tests_main.py
@@ -15,6 +15,17 @@ class TestRecognition(unittest.TestCase):
         r = SpeechRecognition.SpeechRecognition(API_KEY_LOCATION=os.path.join('C:', 'Users', '46709', 'Downloads', 'GAPI.json'), save_audio_files=True)
         self.assertEqual(r.recognize_sync_audio_file('./audio/2020-10-21_17-38-31.raw', "bad_code"), -1)
 
+    ## Unstable test.
+    def test_return_option_all(self):
+        r = SpeechRecognition.SpeechRecognition(API_KEY_LOCATION=os.path.join('C:', 'Users', '46709', 'Downloads', 'GAPI.json'), save_audio_files=True)
+        self.assertEqual(r.recognize_sync_audio_file('./audio/2020-10-21_17-38-31.raw', return_options="all"), 
+"""results {
+  alternatives {
+    transcript: "hello hello hello hello hello hello"
+    confidence: 0.987629
+  }
+}
+""")
 
 
 if __name__ == '__main__':
