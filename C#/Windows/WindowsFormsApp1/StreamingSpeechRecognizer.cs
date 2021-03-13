335617f26c0c511b4b1849c9a963a6617c30523e
JesperMjornman <jesper.mjornman@gmail.com>
2021-03-13 22:20:22 +0100

    initial commit

---
+++ C#/Windows/WindowsFormsApp1/StreamingSpeechRecognizer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using NAudio.Wave;
+using Google.Cloud.Speech.V1;
+using System.IO;
+
+namespace GoogleSpeechToText
+{
+    class StreamingSpeechRecognizer
+    {
+        static void Init(string pathToAPI)
+        {
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", pathToAPI);
+        }
+
+        static async Task<object> StreamingMicrophoneRecognizeAsync(int seconds = 60, string languageCode = "en-US")
+        {
+            var speech = SpeechClient.Create();
+            var streamingCall = speech.StreamingRecognize();
+            await streamingCall.WriteAsync(
+                new StreamingRecognizeRequest()
+                {
+                    StreamingConfig = new StreamingRecognitionConfig()
+                    {
+                        Config = new RecognitionConfig()
+                        {
+                            Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                            SampleRateHertz = 44100,
+                            LanguageCode = languageCode
+                        },
+                        InterimResults = true,
+                    }
+                });
+
+            Task printResponses = Task.Run(async () =>
+            {
+                var responseStream = streamingCall.GetResponseStream();
+                while (await responseStream.MoveNextAsync())
+                {
+                    StreamingRecognizeResponse response = responseStream.Current;
+                    Console.WriteLine(response.Results[0].Alternatives[0].Transcript); // Print most probable result.
+                }
+            });
+
+            object writeLock = new object();
+            bool writeMore = true;
+            var waveIn = new NAudio.Wave.WaveInEvent();
+            waveIn.DeviceNumber = 0;
+            waveIn.WaveFormat = new NAudio.Wave.WaveFormat(44100, 1); // 44100Hz Mono.
+            waveIn.DataAvailable += (object sender, NAudio.Wave.WaveInEventArgs args) =>
+            {
+                lock(writeLock)
+                {
+                    if (!writeMore)
+                        return;
+
+                    streamingCall.WriteAsync(
+                        new StreamingRecognizeRequest()
+                        {
+                            AudioContent = Google.Protobuf.ByteString.CopyFrom(args.Buffer, 0, args.BytesRecorded)
+                        }).Wait();
+                }
+            };
+
+            waveIn.StartRecording();
+            Console.WriteLine("Speek now.");
+            await Task.Delay(TimeSpan.FromSeconds(seconds));
+
+            waveIn.StopRecording();
+            lock(writeLock)
+            {
+                writeMore = false;
+            }
+
+            await streamingCall.WriteCompleteAsync();
+            await printResponses;
+            return 0;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Google Speech Recognition
+Implementation of retreiving sound from a microphone and fetching the Speech To Text using Google Clouds API.
+The ```Python``` implementation allows for "unlimited" length of recordings but the recommended length is still < 1 minute per recording.
+
+Each named folder contains different code implementations (such as C#, Python, ...).
+
+Note that each implementation require an API - key from Google.
+
+## C#
+* [x] Add Asynconous recognition.
+* [ ] Add recognition outside of forms.
+
+## Python
+* [x] Add Asynchronous reognition for larger files.
+* [x] Add support for streaming recognition.
+  - * [x] Make recognition async.
+
+## Warnings
+Should be noted that the latest version is NOT tested as it is from another project. 
+```main.py``` is also outdated and might not reflect an actual implementation example.
